0049a8a6a983d4f1021524fe5ca2ec5209ad538e
pedrinfx <canaldopedrinho233@gmail.com>
2022-10-08 21:37:35 -0300

    deps|fix: removing extra deps | removing upgrade command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use clap::Parser;
-use std::fs::{remove_file, File};
-use std::io::copy;
 
 mod engine;
 mod utils;
@@ -31,56 +29,7 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     if args.commands == Some(Commands::Upgrade) {
-        // This is basically https://github.com/rustbase/rustbase-install/blob/main/install.sh
-        // This only works on x64 Linux ._.
-        let repo = "https://github.com/rustbase/rustbase";
-        let rustbase_download = format!("{}/releases/latest/download/rustbase-linux-x64.zip", repo);
-        let home = home::home_dir().unwrap();
-
-        if systemctl::is_active("rustbase.service").unwrap() {
-            println!("Stopping rustbase service...");
-            systemctl::stop("rustbase.service").unwrap();
-        }
-
-        println!("Upgrading rustbase...");
-
-        let temp_dir = std::path::Path::new(&std::env::temp_dir()).join("rustbase.zip");
-
-        let resp = reqwest::get(rustbase_download)
-            .await
-            .expect("request failed");
-
-        let bytes = resp.bytes().await.expect("failed to read bytes").to_vec();
-
-        let mut out = File::create(temp_dir.clone()).expect("failed to create file");
-
-        copy(&mut bytes.as_slice(), &mut out).expect("failed to copy content");
-
-        zip::ZipArchive::new(File::open(temp_dir.clone()).unwrap())
-            .unwrap()
-            .extract(temp_dir.clone().parent().unwrap())
-            .unwrap();
-
-        std::fs::create_dir_all(home.join("rustbase").join("bin")).unwrap();
-
-        std::fs::rename(
-            temp_dir.clone().parent().unwrap().join("rustbase"),
-            home.join("rustbase").join("bin").join("rustbase_server"),
-        )
-        .unwrap();
-
-        remove_file(temp_dir.clone()).unwrap();
-
-        if let Ok(true) = systemctl::exists("rustbase.service") {
-            systemctl::restart("rustbase.service").unwrap();
-        }
-
-        println!(
-            "{} Rustbase upgraded successfully!",
-            "[Success]".to_string().green()
-        );
-
-        return Ok(());
+        println!("Not implemented yet");
     }
 
     println!("Welcome to Rustbase Shell!");
