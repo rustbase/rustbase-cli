94e80ac11f9e4d3b08c061c26b66a47ba718df5e
pedrinfx <canaldopedrinho233@gmail.com>
2022-11-06 18:38:57 -0300

    feat: compatibility with rustbase v0.2.0

--- src/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["proto/rustbase.proto"], &["proto"])
-        .unwrap();
-}
